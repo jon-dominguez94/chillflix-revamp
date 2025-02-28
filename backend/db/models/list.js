bd0e1eb40797f3f1e6bff47db7af7f19e11fce70
Jon Dominguez <jon.dominguez94@yahoo.com>
2025-02-28 15:34:47 -0800

    Create and test auth middleware

---
+++ backend/db/models/list.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const List = sequelize.define('List', {
+    userId: DataTypes.INTEGER,
+    allowNull: false
+  }, {});
+  List.associate = function(models) {
+    // associations can be defined here
+    List.belongsTo(
+      models.User, {
+        foreignKey: 'userId'
+      }
+    )
+  };
+  return List;
+};
---
+++ backend/db/seeders/20250228225128-demo-user.js
@@ -0,0 +1,43 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@gmail.com',
+        username: 'demo',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'jon@user.io',
+        username: 'jondoom',
+        hashedPassword: bcrypt.hashSync('password2')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['demo', 'jondoom'] }
+    }, {});
+  }
+};
