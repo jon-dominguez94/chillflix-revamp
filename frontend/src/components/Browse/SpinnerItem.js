785d046a455573d4cf576e4c064eb7cb0262c857
Jon Dominguez <jon.dominguez94@yahoo.com>
2025-03-06 22:30:59 -0800

    Implement preview modal

--- frontend/src/components/Browse/SpinnerItem.js
+++ frontend/src/components/Browse/SpinnerItem.js
@@ -1,68 +1,76 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
+import { Modal } from '../../context/Modal';
+import Preview from '../Movies/Preview';
 
 import * as listActions from '../../store/list';
 
 const SpinnerItem = ({movie}) => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const sessionUser = useSelector(state => state.session.user);
     const list = useSelector(state => state.list);
 
-    const [onList, setOnList] = useState(false);
+    const [ onList, setOnList ] = useState(false);
+    const [ showModal, setShowModal ] = useState(false);
 
     useEffect(() => {
         setOnList(movie.id in list);
     }, [list]);
 
     function handleList() {
-    if (onList) {
-        dispatch(listActions.removeFromListThunk(sessionUser.id, movie.id));
-    } else {
-        dispatch(listActions.addToListThunk(sessionUser.id, movie.id));
+        if (onList) {
+            dispatch(listActions.removeFromListThunk(sessionUser.id, movie.id));
+        } else {
+            dispatch(listActions.addToListThunk(sessionUser.id, movie.id));
+        }
     }
-}
+
+    function expand() {
+        setShowModal(true);
+        navigate(`/browse?jbv=${movie.id}`);
+    }
+
+    function collapse() {
+        setShowModal(false);
+        navigate(`/browse`);
+    }
 
     return (
-        <div className="spinner-item enlarge">
-            <img className="movie-tn" src={movie.thumbnail} />
+        <>
+            <div className="spinner-item enlarge">
+                <img className="movie-tn" src={movie.thumbnail} />
 
-            <Link to={`/watch/${movie.id}`}>
-                <div className="play-area"></div>
-            </Link>
+                <Link to={`/watch/${movie.id}`}>
+                    <div className="play-area"></div>
+                </Link>
 
-            {/* <div className={`invisible tn-links`}> */}
-            <div className="center-flex invisible tn-links">
-                {/* <div className="tn-info"> */}
-                    <Link to={`/watch/${movie.id}`}>
-                        <p className="center-flex round-button play-btn">
-                            <i className="fa fa-play"></i>
+                <div className="invisible tn-links">
+                    <div className="center-flex">
+                        <Link to={`/watch/${movie.id}`}>
+                            <p className="center-flex round-button play-btn">
+                                <i className="fa fa-play"></i>
+                            </p>
+                        </Link>
+                        <p className="center-flex round-button plus-btn" onClick={handleList}>
+                            {onList ? <i className="fa fa-check" /> : <i className="fa fa-plus"></i>}
                         </p>
-                    </Link>
-                    {/* <p className="tn-title">{movie.title}</p> */}
-                {/* </div> */}
-
-                {/* <div className="list-adder" onClick={handleList}> */}
-                    {/* <div className="tn-plus-wrapper"> */}
-                        {/* <span>+</span> */}
-                    <p className="center-flex round-button plus-btn">
-                        {onList ? <i className="fa fa-check" /> : <i className="fa fa-plus"></i>}
-
-                    </p>
-                    {/* </div> */}
-                {/* </div> */}
-
-                {/* <div className="info-down"> */}
-                    {/* <Link to={`/browse/${order}/${movie.id}`}> */}
-                <p className="center-flex round-button expand-btn">
-
-                        <i className="fa fa-angle-down"></i>
-                </p>
-                        {/* <i className="fa fa-angle-down" onClick={this.expand}></i> */}
-                    {/* </Link> */}
-                {/* </div> */}
+                        <p className="center-flex round-button expand-btn" onClick={expand} >
+                            <i className="fa fa-angle-down"></i>
+                        </p>
+                    </div>
+                    <p className="tn-title">{movie.title}</p>
+                </div>
+                {showModal && (
+                    <Modal onClose={collapse}>
+                        <Preview movie={movie} onList={onList} onClose={collapse} />
+                    </Modal>
+                )}
             </div>
-        </div>
+
+        </>
     );
 }
 
---
+++ frontend/src/components/Movies/Preview.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import * as listActions from '../../store/list';
+
+
+const Preview = ({ sessionUser, movie, onList, onClose }) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [ toggleList, setToggleList ] = useState(onList)
+
+    useEffect(() => {
+        navigate(`/browse?jbv=${movie.id}`);
+    }, [])
+
+    function handleList() {
+        if (toggleList) {
+            setToggleList(false);
+            dispatch(listActions.removeFromListThunk(sessionUser.id, movie.id));
+        } else {
+            setToggleList(true);
+            dispatch(listActions.addToListThunk(sessionUser.id, movie.id));
+        }
+    }
+
+    return (
+        <>
+            {/* <div className="center-flex curr-movie-wrapper"> */}
+            <div className="preview-container">
+                <div className="video-wrapper">
+                    <video
+                        className=""
+                        width="100%"
+                        height="auto"
+                        src={movie.video}
+                        // controls
+                        autoPlay
+                    >
+                        Your browser does not support the video tag.
+                    </video>
+
+                    <Link to={`/watch/${movie.id}`}>
+                        <div className="play-area"></div>
+                    </Link>
+
+                    <div className="center-flex tn-links">
+                        <Link to={`/watch/${movie.id}`}>
+                            <div className="play-btn">
+                                <div className="main-video-link">
+                                    <button className="button play" />
+                                    <span>Play</span>
+                                </div>
+                            </div>
+                        </Link>
+                        <p className="center-flex round-button plus-btn" onClick={handleList}>
+                            {onList ? <i className="fa fa-check" /> : <i className="fa fa-plus"></i>}
+                        </p>
+                        {/* <p className="center-flex round-button expand-btn" onClick={() => {}} >
+                            <i className="fa fa-angle-down"></i>
+                        </p> */}
+                    </div>
+
+                    <p className="center-flex round-button close-btn" onClick={onClose}>
+                        <i className="fa fa-x"></i>
+                    </p>
+                </div>
+                <div className="bottom-info">
+                    <div className="movie-info">
+                        <p className="preview-title">
+                            {movie.title}
+                        </p>
+                        <p className="preview-description">
+                            {movie.description}
+                        </p>
+                    </div>
+                    <div className="fake-info">
+                        <div className="fake-section">
+                            <p className=""><span className="grey">Cast: </span>Stephen Curry, Draymond Green, Klay Thompson, and <span className='italic'>more</span></p>
+                        </div>
+                        <div className='fake-section'>
+                            <p className=""><span className="grey">Genres: </span>Political TV Shows, TV Shows Based on Books, TV Mysteries</p>
+                        </div>
+                        <div className='fake-section'>
+                            <p className=""><span className="grey">This show is: </span>Exciting, Suspenseful</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Preview;
